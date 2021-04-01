07c5e9a8cddcd4c1e0018246de7069226863d008
Manish Sencha <manishsencha2001@gmail.com>
2021-04-01 13:38:25 +0530

    added more routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const StoreData = require("./data");
 const CartData = require("./cart");
+
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
@@ -16,52 +17,85 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
-  const storeData = await StoreData.find();
-  const cartData = await CartData.find();
-  res.render("index.ejs", { storeData: storeData, cartData: cartData });
-});
-
-app.post("/delete/:id", async (req, res) => {
   try {
-    await StoreData.findByIdAndRemove({ _id: req.params.id });
-    res.redirect("/");
-  } catch (e) {
-    res.redirect("/");
+
+    const storeData = await StoreData.find();
+    const cartData = await CartData.find();
+    res.render("index.ejs", {
+      storeData: storeData,
+      cartData: cartData,
+      message: "",
+    });
+  }catch(e) {
+    res.redirect('/');
   }
 });
 
-app.post("/addtocart/:id", async (req, res) => {
+app.post("/delete/:name", async (req, res) => {
   try {
-    const foundData = await CartData.findOne({ id: req.params.id });
-    const data = await storeData.findOne({ id: req.params.id });
-    if (foundData == null) {
-      await CartData.create({ item: data.item });
-      data.quantity--;
-      data.save();
-    } else {
-      data.quantity--;
-      data.save();
-      foundData.quantity++;
-      foundData.save();
+    await StoreData.findOneAndRemove({ item: req.params.name });
+    res.redirect( '/');
+  } catch (e) {
+    res.redirect('/');
+  }
+});
+
+app.post("/addtocart/:name", async (req, res) => {
+  try {
+    const storeData = await StoreData.findOne({ item: req.params.name });
+    const cartData = await CartData.findOne({ item: req.params.name });
+
+    if (storeData.quantity > 0) {
+      if (cartData == null) {
+        await CartData.create({
+          item: storeData.item,
+        });
+        storeData.quantity--;
+        storeData.save();
+      } else {
+        cartData.quantity++;
+        cartData.save();
+        storeData.quantity--;
+        storeData.save();
+      }
     }
     res.redirect("/");
   } catch (e) {
+    console.log(e);
     res.redirect("/");
   }
 });
-
-app.post("/buy", async (req, res) => {
+app.post("/deletecart/:name", async (req, res) => {
   try {
+    const cartData = await CartData.findOne({ item: req.params.name });
+    const storeData = await StoreData.findOne({ item: req.params.name });
+    if (storeData == null) {
+      await StoreData.create({
+        item: cartData.item,
+        quantity: cartData.quantity,
+      });
+      cartData.remove();
+      res.redirect("/");
+    } else {
+      storeData.quantity += parseInt(cartData.quantity);
+      storeData.save();
+      cartData.remove();
+      cartData.save();
+      res.redirect("/");
+    }
   } catch (e) {
     res.redirect("/");
   }
 });
+
 app.post("/adddata", async (req, res) => {
   try {
-    const dataFound = await StoreData.findOne({ item: req.body.item });
+    const dataFound = await StoreData.findOne({
+      item: req.body.item.trim().toUpperCase(),
+    });
     if (dataFound == null) {
       await StoreData.create({
-        item: req.body.item,
+        item: req.body.item.trim().toUpperCase(),
         quantity: req.body.quantity,
       });
     } else {
